68762c4f41b32a50614e2f440c1f28dd1d9e2451
The KinG Of Rich <batuocbongdem156@gmail.com>
2025-08-20 15:01:11 +0700

    xoa chu viet tay

--- script.js
+++ script.js
@@ -214,7 +214,6 @@ answerBtn.addEventListener('click', () => {
   </div>
   
   <div class="chu_chuan" style="display: flex; justify-content: center; align-items: center;>
-    <div class="textt2">chữ viết tay: </div>
     <div class="chuan_viettay">
       <img src="hiragana/${curr.romaji}.png"/>
     </div>
